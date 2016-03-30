eccbf70e04d51065dace79a8bc54b29c37900acb
Ashley Hutton <ashley.hutton@ku.edu>
2016-03-29 21:15:28 -0500

    Add radio buttons

--- CastVote.java
+++ CastVote.java
@@ -10,23 +10,68 @@ public class CastVote implements ActionListener{
 	JButton button;
 	JLabel firstName;
 	JLabel lastName;
-	JTextField text;
+	JTextField fNameText;
+	JTextField lNameText;
 	JLabel label2;
 
 	public CastVote(){
+
+		String cand1 = "Dog";
+		String cand2 = "Cat";
+		String cand3 = "Pig";
+		String cand4 = "Horse";
+
 
 		panel = new JPanel();
 		button = new JButton("Vote");
 		firstName = new JLabel("First Name:");
 		lastName = new JLabel("Last Name:");
-		text = new JTextField(10);
+		fNameText = new JTextField(10);
+		lNameText = new JTextField(10);
 		label2 = new JLabel();
 
 		button.addActionListener(this);
 
+		JRadioButton cand1Button = new JRadioButton(cand1);
+	    cand1Button.setMnemonic(KeyEvent.VK_C1);
+	    cand1Button.setActionCommand(cand1);
+	    cand1Button.setSelected(true);
+
+	    JRadioButton cand2Button = new JRadioButton(cand2);
+	    cand2Button.setMnemonic(KeyEvent.VK_C2);
+	    cand2Button.setActionCommand(cand2);
+
+	    JRadioButton cand3Button = new JRadioButton(cand3);
+	    cand3Button.setMnemonic(KeyEvent.VK_C3);
+	    cand3Button.setActionCommand(cand3);
+
+	    JRadioButton cand4Button = new JRadioButton(cand4);
+	    cand4Button.setMnemonic(KeyEvent.VK_C4);
+	    cand4Button.setActionCommand(cand4);
+
+	    //Group the radio buttons.
+	    ButtonGroup group = new ButtonGroup();
+	    group.add(birdButton);
+	    group.add(catButton);
+	    group.add(dogButton);
+	    group.add(rabbitButton);
+
+	    //Register a listener for the radio buttons.
+	    birdButton.addActionListener(this);
+	    catButton.addActionListener(this);
+	    dogButton.addActionListener(this);
+	    rabbitButton.addActionListener(this);
+
 		panel.add(firstName);
+		panel.add(fNameText);
 		panel.add(lastName);
-		panel.add(text);
+		panel.add(lNameText);
+
+		panel.add(birdButton);
+		panel.add(catButton);
+		panel.add(dogButton);
+		panel.add(rabbitButton);
+
 		panel.add(button);
 		panel.add(label2);
 
